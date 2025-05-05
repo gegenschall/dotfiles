be2d07b518f5dcfe047b612e3a672c32418ddad1
Max Liebkies <mail@maxliebkies.de>
2025-05-05 12:26:34 +0200

    feat: add golf.vim to nvim

---
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "vuciv/golf",
+    keys = {
+      { "<leader>Gt", "<cmd>Golf today<cr>", desc = "Golf (today's puzzle)" },
+      { "<leader>Ge", "<cmd>Golf easy<cr>", desc = "Golf (easy)" },
+      { "<leader>Gm", "<cmd>Golf medium<cr>", desc = "Golf (medium)" },
+      { "<leader>Gh", "<cmd>Golf hard<cr>", desc = "Golf (hard)" },
+    },
+  },
+}
