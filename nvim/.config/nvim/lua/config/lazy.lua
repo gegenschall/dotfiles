35a753b4fa23702b8354284ed1f9e77ded0e087d
Max Liebkies <mail@maxliebkies.de>
2025-05-05 12:25:51 +0200

    chore: clean up nvim config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -32,11 +32,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.toml" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
 
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,5 +5,6 @@ vim.opt.spelllang = { "en_us" }
 vim.opt.conceallevel = 2
 
 -- vim.g.root_spec = { "pnpm-workspace.yaml", "lsp", { ".git", "lua" }, "cwd" }
-vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.g.lazyvim_picker = "snacks"
+
+vim.g.lazyvim_eslint_auto_format = true
