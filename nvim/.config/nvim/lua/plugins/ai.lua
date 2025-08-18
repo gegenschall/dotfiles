965b93e6c7da3d18c47af46bd885875fe3526414
Max Liebkies <m.liebkies@peter-park.de>
2025-08-18 13:26:39 +0200

    chore: nvim config

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "olimorris/codecompanion.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        auto_trigger = false,
+      },
     },
   },
 }
